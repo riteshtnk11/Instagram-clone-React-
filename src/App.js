d08affdb9f962d12ce97de56372826ea02a958c0
Ritesh <riteshtnk11@gmail.com>
2020-10-03 22:54:48 +0700

    [CHORE] Basic project setup and adding instagram logo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h1>Start to build instagram Clone!</h1>
+
+      {/* Header */}
+      {/* Posts */}
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+/*use rfce as a shortcut for creating base structure for this file.  */
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+         
+        </div>
+    )
+}
+export default Post
