5c67bd914fa36b687b0f80a573fb4ad7eadb6b1c
Ritesh <riteshtnk11@gmail.com>
2020-10-06 19:58:35 +0700

    [CHORE]: Adding Signup functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,144 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { auth, db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  //useState hook react
+  //create state variable and use useState hook react
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    //frontend event listeners.
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      //keeps user logged in on the page refresh also..(backend event listener)
+      if (authUser) {
+        // user has logged in
+        console.log(authUser);
+        setUser(authUser);
+
+        if (authUser.displayName) {
+          //don't update  username
+        } else {
+          //if we just created someone...
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+        //user has logged out
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //To cleanup
+      unsubscribe();
+    };
+  }, [user, username]);
 
   //useEffect runs a piece of code based on specific condition
   useEffect(() => {
     //this is where the code runs
     db.collection("posts").onSnapshot((snapshot) => {
       //onsnapshot => Every time a new post is added this code runs
-      setPosts(snapshot.docs.map((doc) => doc.data())); //maps through each of the doc
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          //maps through each of the doc
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, []); //If [] is blank means it will run once, when the main App component loads and if [posts] is passesd it will run everytime the variable posts changes.
 
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              {" "}
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -25,10 +146,16 @@ function App() {
           alt=""
         />
       </div>
+      {user ? (
+        <Button onClick={() => auth.signOut()}> Log out</Button>
+      ) : (
+        <Button onClick={() => setOpen(true)}> Sign Up</Button>
+      )}
       <h1>Let's build Instagram Clone!</h1>
 
-      {posts.map((post) => (
+      {posts.map(({ id, post }) => (
         <Post
+          key={id} //The unique id of post will render the post with particular id, and not re-render the already rendered post.
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
