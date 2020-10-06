5db766b85eace78fcfa1072e73c0ccb8d6b619e5
Ritesh <riteshtnk11@gmail.com>
2020-10-06 20:25:20 +0700

    [CHORE]: Adding SignIn functionality

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function App() {
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -94,12 +95,23 @@ function App() {
         });
       })
       .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+    setOpenSignIn(false);
   };
 
   return (
     <div className="App">
       <Modal
-        open={open}
+        open={open} //Sign up modal
         onClose={() => setOpen(false)}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
@@ -139,6 +151,41 @@ function App() {
         </div>
       </Modal>
 
+      <Modal
+        open={openSignIn} //Sign In modal
+        onClose={() => setOpenSignIn(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>
+              {" "}
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -149,7 +196,10 @@ function App() {
       {user ? (
         <Button onClick={() => auth.signOut()}> Log out</Button>
       ) : (
-        <Button onClick={() => setOpen(true)}> Sign Up</Button>
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}> Sign In</Button>
+          <Button onClick={() => setOpen(true)}> Sign Up</Button>
+        </div>
       )}
       <h1>Let's build Instagram Clone!</h1>
 
